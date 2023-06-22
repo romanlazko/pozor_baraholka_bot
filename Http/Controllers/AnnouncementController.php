28aa28522a9213ec83b28ae81d8d789c5ad10040
romanlazko <romanlazko@gmail.com>
2023-06-22 12:15:13 +0200

    Испраление ошибок

--- Http/Controllers/AnnouncementController.php
+++ Http/Controllers/AnnouncementController.php
@@ -27,7 +27,9 @@ class AnnouncementController extends Controller
         $announcements->map(function ($announcement) use ($telegram){
             $announcement->chat = $announcement->chat()->first();
             $announcement->chat->photo = $telegram::getPhoto(['file_id' => $announcement->chat->photo]);
-            $announcement->photo = $telegram::getPhoto(['file_id' => $announcement->chat->photo]);
+            $announcement->photos = $announcement->photos->map(function ($photo) use ($telegram){
+                $photo->url = $telegram::getPhoto(['file_id' => $photo->file_id]);
+            });
             return $announcement;
         });
 
